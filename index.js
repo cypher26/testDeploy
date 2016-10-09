83a3c6ea9f6777604b10a4c9a7ffcd21df39195f
cypher26 <jestercaporado@yahoo.com>
2016-10-09 16:57:14 +0800

    testule

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 var router = express.Router();
 var path = __dirname + '/views/';
 
@@ -35,8 +37,9 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(app.get('port'), function() {
+console.log('Node app is running on port', app.get('port'));
+
 });
 
 

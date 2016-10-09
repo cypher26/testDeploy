c2ada2d87e01ecf6bf64f62ccca10877ed44b123
cypher26 <jestercaporado@yahoo.com>
2016-10-09 16:50:11 +0800

    test

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var router = express.Router();
+var path = __dirname + '/views/';
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-
-	console.log('asdkfja;slkdjfa;sljfd"0');
-app.get('/', function(request, response) {
-	console.log("hi jester pogi");
-  response.render('pages/index');
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(app.get('port'), function() {
-	  console.log('Nasdfasfasdfasdfasf');
-  console.log('Node app is running on port', app.get('port'));
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
 
+// router.get("/about",function(req,res){
+//   res.sendFile(path + "about.html");
+// });
+
+// router.get("/contact",function(req,res){
+//   res.sendFile(path + "contact.html");
+// });
+
+// router.get("/js/jquery",function(req,res){
+//   res.sendFile(path + "/js/jquery.js");
+// });
+
+
+app.use("",express.static(__dirname + '/views'));
+app.use("js",express.static(__dirname + '/js'));
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
 });
 
 
